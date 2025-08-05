319e441fc99bcf7b4bfe3254a0941139d0171e1c
petertechy <petertechy01@gmail.com>
2025-08-05 12:21:28 +0100

    Add NotFound.jsx

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1 className='text-center my-3 display-1 fw-bold text-danger'>Not Found Page</h1>
+    </div>
+  )
+}
+
+export default NotFound
