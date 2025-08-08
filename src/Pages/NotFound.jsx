916042381a5bdc94c63a4b9aaaad4d4b749af156
petertechy <petertechy01@gmail.com>
2025-08-08 23:48:02 +0100

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="mx-auto col-7 p-3 my-4">
         <h1 className='text-center my-3 display-1 fw-bold text-danger'>Not Found Page</h1>
     </div>
   )
