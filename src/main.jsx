95eea5566f5d06d4b9df8423e6bf80d7b21eaf17
petertechy <petertechy01@gmail.com>
2025-08-11 23:05:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from './App.jsx'
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import { BrowserRouter } from 'react-router-dom'
+import App from "./App.jsx";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
