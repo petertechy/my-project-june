1e0d45c28841e7cb9564d5a470dd0a6a2d8af5a7
petertechy <petertechy01@gmail.com>
2025-08-12 11:35:12 +0100

    Update counterSlice.js

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -5,8 +5,25 @@ export const counterSlice = createSlice({
     name: 'counter',
     initialState: {
         count: 2,
-        allStudent: []
+        allStudent: JSON.parse(localStorage.getItem("student")) || []
+    },
+    reducers: {
+        increment: (state)=>{
+           state.count += 1
+        },
+        decrement: (state)=>{
+            state.count -= 1
+        },
+        increaseByParam: (state, actions) =>{
+            state.count += actions.payload
+        },
+        saveStudent: (state, actions)=>{
+            state.allStudent.push(actions.payload)
+
+            localStorage.setItem('student', JSON.stringify(state.allStudent))
+        }
     }
 })
 
+export const {increment, decrement, increaseByParam, saveStudent} = counterSlice.actions
 export default counterSlice.reducer
